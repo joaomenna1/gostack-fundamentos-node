9444578ca105992db9e8894789dd2435055c6df1
joao nogueira <joaomenna1@gmail.com>
2020-04-21 18:45:50 -0400

    done !

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+enum Type {
+  INCOME = 'income',
+  OUTCOME = 'outcome',
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -13,14 +24,37 @@ class TransactionsRepository {
     this.transactions = [];
   }
 
-  public all(): Transaction[] {}
-
-  public getBalance(): Balance {
-    // TODO
+  public all(): Transaction[] {
+    return this.transactions;
   }
 
-  public create(): Transaction {
-    // TODO
+  private calculateBalance(operator: Type): number {
+    return this.transactions
+      .filter(t => t.type === operator)
+      .reduce((acc, tr) => acc + tr.value, 0);
+  }
+
+  public getBalance(): Balance {
+    const income = this.calculateBalance(Type.INCOME);
+    const outcome = this.calculateBalance(Type.OUTCOME);
+    const total = income - outcome;
+
+    const balance: Balance = { income, outcome, total };
+
+    return balance;
+  }
+
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    const { total } = this.getBalance();
+    if (type === Type.OUTCOME && total < value) {
+      throw Error('Cannot create outcome transaction without a valid balance');
+    }
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
